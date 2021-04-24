af06bcb051e24f643f74f058919bf0e04fb7b9ee
biel-rds <gabriel.rds.developer@gmail.com>
2021-04-24 11:39:27 -0300

    include piece

--- src/aplication/Program.java
+++ src/aplication/Program.java
@@ -1,10 +1,11 @@
 package aplication;
 
+import boardgame.Board;
+
 public class Program {
 
 	public static void main(String[] args) {
-		System.out.println("teste");
-
+		Board board = new Board(8, 8);
 	}
 
 }
---
+++ src/boardgame/Piece.java
@@ -0,0 +1,17 @@
+package boardgame;
+
+public class Piece {
+	protected Position postion;
+	private Board board;
+	
+	public Piece(Board board) {		
+		this.board = board;
+		postion = null;
+	}
+
+	protected Board getBoard() {
+		return board;
+	}	
+	
+	
+}
